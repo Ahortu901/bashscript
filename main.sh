38786667aaebe16f748fb59d7663615c0575c5a4
Ahortu901 <aricktech.edu@gmail.com>
2023-10-29 20:39:26 +0000

    updating

--- main.sh
+++ main.sh
@@ -3,10 +3,10 @@
 if [ ${1,,} = derrick ]; then
     echo "Oh, you're the boss here. Welcome!"
 elif [ ${1,,} = help ]; then
-    echo "Enter your name: "
+    echo "[+] Enter your name: "
     read username
-    echo "Hello $username, you are Welcome"
+    echo "[+] Hello $username, you are Welcome"
 
 else
-    echo "I don't know you"
+    echo "[-] I don't know you"
 fi
