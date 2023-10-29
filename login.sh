8686e6ff912afc6c497d800233f962b2af40f8ec
Ahortu901 <aricktech.edu@gmail.com>
2023-10-29 20:57:33 +0000

    updating files

---
+++ login.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+case ${1,,} in
+    Admin | admin)
+        printf "Welcome to the adminstrator terminal\nHappy coding -_0"
+        ;;
+    help)
+        printf "To use this program please add an aggurment to command\nExample:\n=======\n./login.sh username\n============\nHappy Coding -_0"
+        ;;
+    *)
+        printf "Please Enter a valid username!"
+        read username
+        echo ""
+        echo "Welcome $username!"
+esac
